04f665778ef0a5e62ee85df6a3a5bb5eefdb7f2e
roy_wang <roy_wang@paytronex.com>
2025-08-01 17:33:28 +0800

    [project-11] Build AuthContext

---
+++ 11-worldwise/src/contexts/AuthContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+    user: null,
+    isAuthenticated: false,
+};
+
+const FAKE_USER = {
+    name: "Jack",
+    email: "jack@example.com",
+    password: "qwerty",
+    avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "login":
+            return { ...state, user: action.payload, isAuthenticated: true };
+        case "logout":
+            return { ...state, user: null, isAuthenticated: false };
+        default:
+            throw new Error("action.type error of AutContext");
+    }
+}
+
+function AuthProvider({ children }) {
+    const [{ user, isAuthenticated }, dispatch] = useReducer(reducer, initialState);
+    function login(email, pwd) {
+        if (email === FAKE_USER.email && pwd === FAKE_USER.password) {
+            dispatch({ type: "login", payload: FAKE_USER });
+        }
+    }
+    function logout() {
+        dispatch({ type: "logout" });
+    }
+    return (
+        <AuthContext
+            value={{
+                user,
+                isAuthenticated,
+                login,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext>
+    );
+}
+
+function useAuth() {
+    const context = useContext(AuthContext);
+    if (context === undefined) throw new Error("CitiesContext was used outside of the CitiesProvider");
+    return context;
+}
+
+export { AuthProvider, useAuth };
