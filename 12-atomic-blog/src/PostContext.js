aa579041e55516ab1896a5cb8d38128d004b19af
roy_wang <roy_wang@paytronex.com>
2025-08-06 15:43:07 +0800

    Improve performance by memo

--- 12-atomic-blog/src/PostContext.js
+++ 12-atomic-blog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -30,19 +30,19 @@ function PostProvider({ children }) {
         setPosts([]);
     }
 
+    const value = useMemo(() => {
+        return {
+            posts: searchedPosts,
+            onClearPosts: handleClearPosts,
+            searchQuery,
+            setSearchQuery,
+            onAddPost: handleAddPost,
+        };
+    }, [searchedPosts, searchQuery]);
+
     return (
         // 2) PROVIDE VALUE TO CHILD COMPONENTS
-        <PostContext.Provider
-            value={{
-                posts: searchedPosts,
-                onClearPosts: handleClearPosts,
-                searchQuery,
-                setSearchQuery,
-                onAddPost: handleAddPost,
-            }}
-        >
-            {children}
-        </PostContext.Provider>
+        <PostContext.Provider value={value}>{children}</PostContext.Provider>
     );
 }
 
