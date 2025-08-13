320cf81303a727cdcd9ab53b8ffce652b1844037
roy_wang <roy_wang@paytronex.com>
2025-08-13 17:03:36 +0800

    Bring back thunk middleware with redux-toolkit

--- 15-redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/src/features/accounts/accountSlice.js
@@ -15,6 +15,7 @@ const accountSlice = createSlice({
         deposit(state, action) {
             // different with classic: write by mutated way
             state.balance += action.payload;
+            state.isLoading = false;
         },
         withdraw(state, action) {
             state.balance -= action.payload;
@@ -38,10 +39,37 @@ const accountSlice = createSlice({
             state.loan = 0;
             state.loanPurpose = "";
         },
+        convertingCurrency(state) {
+            state.isLoading = true;
+        }
     },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+// 1. CreateAsyncThunk 
+// 2. export classic redux thunk function
+export function deposit(amount, currency) {
+    if (currency === "USD")
+        return {
+            type: "account/deposit",
+            payload: amount,
+        };
+    // Return a function because of using thunk middleware
+    return async function (dispatch, getState) {
+        dispatch({ type: "account/convertingCurrency" });
+        // API call
+        const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+        );
+        const data = await res.json();
+        const converted = data.rates.USD;
+        dispatch({
+            type: "account/deposit",
+            payload: converted,
+        });
+    };
+}
 
 export default accountSlice.reducer;
 
