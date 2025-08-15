245cb038ef1c3e1cebd5e238fe9efb15ef321116
roy_wang <roy_wang@paytronex.com>
2025-08-15 17:46:50 +0800

    Use react-router action to POST data

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { Form, redirect } from 'react-router-dom';
+import { createOrder } from '../../service/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +39,7 @@ function CreateOrder() {
         <div>
             <h2>Ready to order? Let's go!</h2>
 
-            <form>
+            <Form method="POST" action="">
                 <div>
                     <label>First Name</label>
                     <input type="text" name="customer" required />
@@ -69,14 +70,33 @@ function CreateOrder() {
                     <label htmlFor="priority">
                         Want to yo give your order priority?
                     </label>
+                    <input
+                        type="hidden"
+                        name="cart"
+                        value={JSON.stringify(cart)}
+                    />
                 </div>
 
                 <div>
                     <button>Order now</button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
+}
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const order = {
+        ...data,
+        cart: JSON.parse(data.cart),
+        priority: data.priority === 'on',
+    };
+
+    const newOrder = await createOrder(order);
+
+    return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
