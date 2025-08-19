7dba4a478db0ecba8cd0b4a5bcca477a8681bc40
roy_wang <roy_wang@paytronex.com>
2025-08-19 17:51:29 +0800

    Handling data validation by using useActionData

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../service/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
@@ -32,6 +32,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+    const formErrors = useActionData();
     // const [withPriority, setWithPriority] = useState(false);
     const cart = fakeCart;
 
@@ -50,6 +53,7 @@ function CreateOrder() {
                     <div>
                         <input type="tel" name="phone" required />
                     </div>
+                    {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
 
                 <div>
@@ -78,7 +82,9 @@ function CreateOrder() {
                 </div>
 
                 <div>
-                    <button>Order now</button>
+                    <button disabled={isSubmitting}>
+                        {isSubmitting ? 'Placing order ...' : 'Order now'}
+                    </button>
                 </div>
             </Form>
         </div>
@@ -93,6 +99,14 @@ export async function action({ request }) {
         cart: JSON.parse(data.cart),
         priority: data.priority === 'on',
     };
+
+    const errors = {};
+
+    if (!isValidPhone(order.phone)) {
+        errors.phone = 'Wrong phone number 🤯';
+    }
+
+    if (Object.keys(errors).length > 0) return errors;
 
     const newOrder = await createOrder(order);
 
